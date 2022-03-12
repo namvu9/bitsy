d817cdcc06247d5b2777a0d8210ed9fca5b2f909
Nam Vu <vuna@protonmail.com>
2022-03-12 09:39:17 +0100

    Add descriptive struct comments

--- internal/session/cfg.go
+++ internal/session/cfg.go
@@ -1,5 +1,7 @@
 package session
 
+// Config represents the configuration of stuff.
+// Not really important
 type Config struct {
 	BaseDir        string
 	DownloadDir    string // Where to write the completed files to
